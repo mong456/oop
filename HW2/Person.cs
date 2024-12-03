72cc9395de0d10a46f4f4206eb85beace412a849
mong456 <nguyenmongmong162005@gmail.com>
2024-12-03 09:44:24 +0700

    Add HW2

---
+++ HW2/Person.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace hw2
+{
+    internal class Person
+    {
+        private string name { get; set; }
+        private string address { get; set; }
+        private double salary { get; set; }
+
+        public Person() : this("", "", 0.0) { }
+        public Person(string name, string address, double salary)
+        {
+            this.name = name;
+            this.address = address;
+            this.salary = salary;
+        }
+
+        public Person inputPersonInfor()//Enter information from user
+        {
+            while (true)
+            {
+                Console.Write("Name: ");
+                name= Console.ReadLine();
+                if (name != "")
+                    break;              
+            }
+            while (true)
+            {
+                Console.Write("Address: ");
+                address = Console.ReadLine();
+                if (address != "")
+                    break;
+            }
+            while (true)
+            {
+                Console.Write("Salary: ");
+                salary = double.Parse(Console.ReadLine());
+                if (!double.TryParse(salary.ToString(), out double parsedSalary))//Check salary is valid or not 
+                {                               
+                    Console.WriteLine($"'{salary}' is not valid. Enter again:");        
+                }
+                if(salary <0)
+                {
+                    Console.WriteLine("Salary must be a positive number");
+                }
+                else
+                {
+                    this.salary = parsedSalary;
+                    break;
+                }
+            }
+
+            return new Person(name, address, salary);
+        }
+
+        public static void dispalyPersonInfor(Person person)
+        {
+            Console.WriteLine($"{person.name} - {person.address} - {person.salary}");
+        }
+
+        public static Person[] SortBySalary(Person[] persons)//Short person 
+        {
+            int n = persons.Length;
+            if (n < 0)
+                Console.WriteLine("Can't sort Person");
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (persons[j].salary > persons[j + 1].salary)
+                    {
+                        // Hoán đổi hai phần tử nếu lương của phần tử trước lớn hơn phần tử sau
+                        Person temp = persons[j];
+                        persons[j] = persons[j + 1];
+                        persons[j + 1] = temp;
+                    }
+                }
+            }
+            return persons;
+        }
+    } 
+   
+}
---
+++ HW2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program(); // Khởi tạo đối tượng Program
+            program.Option(); 
+
+            
+        }
+
+        public void Option()
+        {
+            bool exit = false;
+            Person[] persons = new Person[0]; // Mảng person ban đầu trống
+
+            while (!exit)
+            {
+                // Hiển thị menu
+                Console.Clear();
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Enter person information");
+                Console.WriteLine("2. Display person information");
+                Console.WriteLine("3. Sort persons by salary");
+                Console.WriteLine("4. Exit");
+                Console.Write("Please choose an option (1-4): ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        // Nhập thông tin cho người
+                        Console.Write("Enter number of persons: ");
+                        int n = int.Parse(Console.ReadLine());
+                        persons = new Person[n]; // Khởi tạo lại mảng với kích thước n
+
+                        for (int i = 0; i < n; i++) // Nhập thông tin cho từng person
+                        {
+                            Console.WriteLine($"Person {i + 1}: ");
+                            Person p = new Person();
+                            persons[i] = p.inputPersonInfor(); // Gọi phương thức nhập thông tin
+                        }
+                        break;
+
+                    case "2":
+                        // Hiển thị thông tin của tất cả người
+                        Console.WriteLine("\nDisplaying Person Information:\n");
+                        foreach (var person in persons)
+                        {
+                            Person.dispalyPersonInfor(person); // Hiển thị thông tin từng person
+                            Console.WriteLine(); // Thêm dòng trống giữa các thông tin người
+                        }
+                        break;
+
+                    case "3":
+                        // Sắp xếp mảng người theo lương
+                        if (persons.Length > 0)
+                        {
+                            foreach (var pe in persons)
+                            {
+                                Person.SortBySalary(persons);
+                                Person.dispalyPersonInfor(pe);
+                                Console.WriteLine();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No persons to sort.");
+                        }
+                        break;
+
+                    case "4":
+                        // Thoát chương trình
+                        exit = true;
+                        Console.WriteLine("Exiting program.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid option, please choose again.");
+                        break;
+                }
+
+                // Đợi người dùng nhấn phím để quay lại menu
+                if (!exit)
+                {
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+    }
+}
