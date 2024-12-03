1051a24c8ac95258e3c26ddb6355d6219eda9920
mong456 <nguyenmongmong162005@gmail.com>
2024-12-03 09:43:06 +0700

    Add HW1

---
+++ HW1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hoTen = "Nguyen Xuan Mong";       
+            string maSinhVien = "12423024";       
+            string lop = "12423TN";                
+            string githubUsername = "mong456";   
+            string email = "nguyenmongmong162005@gmail.com";  
+
+            Console.WriteLine($"{hoTen}\t{maSinhVien}\t{lop}\t{githubUsername}\t{email}");
+        }
+    }
+}
