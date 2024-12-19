dda29bff7b000d951d0d8b469ea216e6b8acc6d5
mong456 <nguyenmongmong162005@gmail.com>
2024-12-19 10:42:54 +0700

    Add HW3

---
+++ HW3/BillLine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    internal class BillLine
+    {
+        public Item item { get; set; }
+        public int quantity { get; set; }
+
+        public BillLine() { }
+        public BillLine(Item item, int quantity)
+        {
+            this.item = item;
+            this.quantity = quantity;
+        }
+
+        public double GetLineTotal()
+        {
+            if (item == null)
+            {
+                throw new InvalidOperationException("Item is not initialized.");
+            }
+
+            return quantity * item.getPrice();
+        }
+
+
+        public override string ToString()
+        {
+            return $"{item.getIdItem()} : {quantity}  have total price ${GetLineTotal()}";
+        }
+    }
+}
---
+++ HW3/GroceryBill.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    internal class GroceryBill
+    {
+        public Employee clerk;
+        protected List<Item> items= new List<Item>();
+        //update
+        protected List<BillLine> lines= new List<BillLine>();       
+        
+        public GroceryBill() { }
+        public GroceryBill(Employee clerk)
+        {
+            this.clerk = clerk;
+        }
+
+        public void Add(Item i) {
+            if (DuplicateID(i.getIdItem()) != true)
+                items.Add(i);
+            else
+                Console.WriteLine($"Exist item {i.getIdItem()} in list");
+        }
+        public bool DuplicateID(string id)
+        {
+            bool flag=false;
+            foreach (Item i in items) {
+                if (i.getIdItem() == id.Trim())
+                    flag = true;break;
+            }
+            return flag;
+        }
+
+        public virtual double getTotal()
+        {
+            double total = 0; 
+            foreach(Item i in items)
+            {
+                total += i.getPrice();
+            }
+            return total;
+        }
+        
+
+        public virtual void printReceipt()
+        {
+           Console.WriteLine(items.ToString());
+        }
+
+        //Update
+        public void Add(BillLine billLine)
+        {
+            // Duyệt qua từng phần tử trong danh sách lines để kiểm tra xem item đã có chưa
+            foreach (var existingBillLine in lines)
+            {
+                if (existingBillLine.item.Equals(billLine.item))
+                {
+                    // Nếu đã có item trong danh sách, tăng số lượng
+                    existingBillLine.quantity += billLine.quantity;
+                    return; // Dừng lại vì đã tìm thấy item và cập nhật số lượng
+                }
+            }
+
+            // Nếu không tìm thấy item trong danh sách, thêm billLine mới vào
+            lines.Add(billLine);
+        }
+
+
+        //Lấy tổng trong billLine
+        public double GetTotal()
+        {
+            double total = 0.0;
+            foreach (var billLine in lines)
+            {
+                total += billLine.GetLineTotal();
+            }
+            return total;
+        }
+        public void printReceipt1()//In thông tin danh sách billLine
+        {
+            foreach (var billLine in lines)
+            {
+                Console.WriteLine(billLine);
+            }
+        }
+
+    }
+}
